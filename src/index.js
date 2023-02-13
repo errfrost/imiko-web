bec084ad53a400d313115bd4bbba3d449419b2f0
Vladimir Kurganov <err0rfrost@gmail.com>
2023-02-13 20:45:03 +0600

    feat: combo arrows

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const vacancy = document.getElementById("vacancy");
 const dropbox = document.getElementById("vacancy-box");
 const list = document.getElementsByName("vacancy-item");
-vacancy.addEventListener("click", () => {
+const inputIcon = document.querySelector(".input-icon");
+
+function clickCombo() {
   dropbox.click();
-});
+  if (inputIcon.classList.contains("rotate")) {
+    inputIcon.classList.remove("rotate");
+  } else {
+    inputIcon.classList.add("rotate");
+  }
+}
+
+vacancy.addEventListener("click", clickCombo);
+inputIcon.addEventListener("click", clickCombo);
 
 vacancy.addEventListener("onkeypress", (e) => {
   e.preventDefault();
 });
 
 for (let i = 0; i < list.length; i++) {
-  list[i].addEventListener('click', (e) => {
+  list[i].addEventListener("click", (e) => {
     vacancy.value = e.target.title;
-    dropbox.click();
+    clickCombo();
   });
 }
